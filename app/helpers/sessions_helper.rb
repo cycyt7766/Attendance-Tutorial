bbab50314e250a15a6ee3d4ca567dc55d41f369c
豊田 雄作 <yusaku.t.oit.a19060@gmail.com>
2020-06-10 13:21:14 +0000

    ユーザー編集ページ作成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,9 +30,9 @@ module SessionsHelper
   # それ以外の場合はcookiesに対応するユーザーを返します。
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id :user_id)
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      user = USer.find_by(id: user_id)
+      user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
         @current_user = user
